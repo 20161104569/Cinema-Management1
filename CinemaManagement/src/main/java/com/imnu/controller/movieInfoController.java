72a2ba04b5a58f7fcb98e052dfd5f9e35c27c1be
20161104569 <2174037646@qq.com>
2020-02-01 18:28:25 +0800

    后端代码2
    后端代码2

---
+++ CinemaManagement/src/main/java/com/imnu/controller/movieInfoController.java
@@ -0,0 +1,89 @@
+package com.imnu.controller;
+
+import java.sql.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.model.Person;
+import com.project.model.movieInfo;
+import com.project.service.movieInfoService;
+
+@RestController
+@RequestMapping("/movieInfoController")
+public class movieInfoController {
+	
+	private movieInfoService movieInfoService;
+
+	public movieInfoService getMovieInfoService() {
+		return movieInfoService;
+	}
+
+	@Autowired
+	public void setMovieInfoService(movieInfoService movieInfoService) {
+		this.movieInfoService = movieInfoService;
+	}
+	
+	@RequestMapping("/allMovieInfo")
+    @ResponseBody
+    public List<movieInfo> allMovieInfo(){
+    	List<movieInfo> movieList=movieInfoService.allMovieInfo();
+        return movieList;
+    }
+    
+    @RequestMapping("/addMovieInfo")
+    @ResponseBody
+    public void addMovieInfo(String moviename,String content,Date starttime,Date endtime,String state,int sumticket){
+    	movieInfo movieInfo =new movieInfo();
+    	movieInfo.setMoviename(moviename);
+    	movieInfo.setContent(content);
+    	movieInfo.setStarttime(starttime);
+    	movieInfo.setEndtime(endtime);
+    	movieInfo.setState(state);
+    	movieInfo.setSumticket(sumticket);
+    	movieInfo.setPurchasedticket(0);
+    	movieInfo.setSurplusticket(sumticket);
+    	movieInfoService.addMovieInfo(movieInfo);
+    }
+    
+    @RequestMapping("/delMovieInfo")
+    @ResponseBody
+    public void delMovieInfo(int id){
+    	movieInfo movieInfo =new movieInfo();
+    	movieInfo.setId(id);
+    	movieInfoService.delMovieInfo(movieInfo);
+    }
+    
+    @RequestMapping("/updateMovieInfo")
+    @ResponseBody
+    public void updateMovieInfo(int id,String moviename,String content,Date starttime,Date endtime,String state,int sumticket){
+    	System.out.println(moviename);
+    	movieInfo movieInfo =new movieInfo();
+    	movieInfo.setId(id);
+    	movieInfo.setMoviename(moviename);
+    	movieInfo.setContent(content);
+    	movieInfo.setStarttime(starttime);
+    	movieInfo.setEndtime(endtime);
+    	movieInfo.setState(state);
+    	movieInfo.setSumticket(sumticket);
+    	movieInfoService.updateMovieInfo(movieInfo);
+    }
+    
+    @RequestMapping("/queryMovieInfoById")
+    @ResponseBody
+    public movieInfo queryMovieInfoById(int id){
+    	movieInfo movieInfo=movieInfoService.queryMovieInfoById(id);
+        return movieInfo;
+    }
+    
+    @RequestMapping("/queryMoiveForName")
+    @ResponseBody
+    public List<movieInfo> queryMoiveForName(String moivename){
+    	List<movieInfo> person=movieInfoService.queryMoiveForName(moivename);
+        return person;
+    }
+
+}
---
+++ CinemaManagement/src/main/java/com/imnu/service/PersonService.java
@@ -0,0 +1,59 @@
+package com.imnu.service;
+
+
+import java.util.List;
+
+import com.project.model.Person;
+public interface PersonService {
+    
+    /**
+     * 登录
+     * @return
+     */
+    Person login(Person person);
+    
+    /**
+     * 注册
+     * @return
+     */
+    int register(Person person);
+    
+    /**
+     * 查询所有工作人员信息
+     * @return
+     */
+    List<Person> allPerson();
+    
+    /**
+     * 添加工作人员
+     * @param person
+     * @return
+     */
+    void addPerson(Person person);
+    
+    /**
+     * 删除工作人员
+     * @param person
+     * @return
+     */
+    void delPerson(Person person);
+    
+    /**
+     * 修改工作人员
+     * @param person
+     * @return
+     */
+    void updatePerson(Person person);
+    
+    /**
+     * 根据id查询工作人员信息
+     * @return
+     */
+    Person queryPersonById(int id);
+    
+    /**
+     * 根据name查询工作人员信息
+     * @return
+     */
+    List<Person> queryPersonForName(String name);
+}
